0af2eab862c0011a4938e27bdc7b89d83a17b554
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-02-10 16:14:34 -0300

    cap 5 - Potential Third-Party Option - Adding droid controller

--- src/main/java/com/mark/heckler/examples/ExamplesApplication.java
+++ src/main/java/com/mark/heckler/examples/ExamplesApplication.java
@@ -1,9 +1,12 @@
 package com.mark.heckler.examples;
 
 
+import com.mark.heckler.examples.config.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -13,5 +16,12 @@ public class ExamplesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExamplesApplication.class, args);
 	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "droid")
+	Droid createDroid(){
+		return new Droid();
+	}
+
 
 }
---
+++ src/main/java/com/mark/heckler/examples/config/Droid.java
@@ -0,0 +1,21 @@
+package com.mark.heckler.examples.config;
+
+public class Droid {
+    private String id, description;
+
+    public String getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
